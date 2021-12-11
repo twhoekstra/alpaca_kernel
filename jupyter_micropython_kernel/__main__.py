f900a4f0f843144271d837b76aa01d1ba1fa62e1
twhoekstra <t.w.hoekstra@student.tudelft.nl>
2021-12-11 22:04:15 +0100

    Uploaded edit to kernel

--- jupyter_micropython_kernel/__main__.py
+++ jupyter_micropython_kernel/__main__.py
@@ -4,6 +4,6 @@ import sys
 logging.basicConfig(level=logging.DEBUG)
 
 from ipykernel.kernelapp import IPKernelApp
-from .kernel import MicroPythonKernel
-IPKernelApp.launch_instance(kernel_class=MicroPythonKernel)
+from .kernel import ALPACAKernel
+IPKernelApp.launch_instance(kernel_class=ALPACAKernel)
 
