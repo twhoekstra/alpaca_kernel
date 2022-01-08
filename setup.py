eb41d4b96b67bd8c73479878697353a27600aa5d
twhoekstra <t.w.hoekstra@student.tudelft.nl>
2022-01-08 16:37:58 +0100

    Updated requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='alpaca_kernel',
-    version='0.1.7',
+    version='0.1.8',
     author='Thijn Hoekstra',
     author_email='t.w.hoekstra@student.tudelft.nl',
     description='Fork of jupyter_micropython_kernel by goatchurchprime',
@@ -21,5 +21,6 @@ setuptools.setup(
         'matplotlib>=3.4.3'
         'numpy>=1.21.2'
         'ipykernel>=5.5.5'
+        'jupyter >= 1.0.0'
     ],
 )
