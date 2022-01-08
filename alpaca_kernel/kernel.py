002c102ebe2be89a5321683424e02a23ed493e4b
twhoekstra <t.w.hoekstra@student.tudelft.nl>
2022-01-08 18:46:39 +0100

    Specified python build version in meta.yaml

--- alpaca_kernel/kernel.py
+++ alpaca_kernel/kernel.py
@@ -248,7 +248,7 @@ def unpack_Thonny_string(output):
 
 class ALPACAKernel(Kernel):
     implementation = 'alpaca_kernel'
-    implementation_version = "v0.1.12"
+    implementation_version = "v0.1.13"
 
     banner = "MicroPython Serializer for ALPACA"
 
