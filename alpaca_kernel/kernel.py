f998dd1a152e3c171fdea81b479f8bb658f85ec3
twhoekstra <t.w.hoekstra@student.tudelft.nl>
2022-01-08 17:01:32 +0100

    Recommit

--- alpaca_kernel/kernel.py
+++ alpaca_kernel/kernel.py
@@ -248,7 +248,7 @@ def unpack_Thonny_string(output):
 
 class ALPACAKernel(Kernel):
     implementation = 'alpaca_kernel'
-    implementation_version = "v0.1.8"
+    implementation_version = "v0.1.9"
 
     banner = "MicroPython Serializer for ALPACA"
 
