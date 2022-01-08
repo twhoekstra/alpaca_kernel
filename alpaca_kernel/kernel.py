0f6c4888bc80c1d2e0b30085f6f1c276ee808753
twhoekstra <t.w.hoekstra@student.tudelft.nl>
2022-01-08 13:57:28 +0100

    Debugging roll

--- alpaca_kernel/kernel.py
+++ alpaca_kernel/kernel.py
@@ -248,7 +248,7 @@ def unpack_Thonny_string(output):
 
 class ALPACAKernel(Kernel):
     implementation = 'alpaca_kernel'
-    implementation_version = "v0.1.3"
+    implementation_version = "v0.1.4"
 
     banner = "MicroPython Serializer for ALPACA"
 
